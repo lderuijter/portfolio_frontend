15d6209bd48e4c91f475344d1bd76bc1657ea75b
lderuijter <lderuijter2@student.scalda.nl>
2025-02-21 22:35:13 +0100

    changed api url to local again

--- src/components/ProjectOverview.vue
+++ src/components/ProjectOverview.vue
@@ -11,7 +11,7 @@ interface Project {
 const projects = ref<Project[]>([]);
 
 onMounted(async () => {
-  const response = await axios.get('https://portfolio-backend-cnv6.onrender.com/api/projects');
+  const response = await axios.get('http://localhost/api/projects');
   projects.value = response.data;
 });
 </script>
