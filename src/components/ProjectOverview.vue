ef8113dbc8918156d143eb319e108a1a322a474b
lderuijter <lderuijter2@student.scalda.nl>
2025-02-21 19:27:00 +0100

    created the projects page to fetch all of the projects from the laravel back end api

---
+++ src/components/ProjectOverview.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+interface Project {
+  id: number;
+  title: string;
+  description: string;
+}
+
+const projects = ref<Project[]>([]);
+
+onMounted(async () => {
+  const response = await axios.get('http://localhost:8000/api/projects');
+  projects.value = response.data;
+});
+</script>
+
+<template>
+  <div>
+    <h2 class="text-2xl font-bold">Projects</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h3 class="text-xl">{{ project.title }}</h3>
+        <p class="text-gray-500">{{ project.description }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
